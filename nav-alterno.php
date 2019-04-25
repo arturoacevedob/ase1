961fd65807866825092335a599d56be1f5231c5d
Mathias Thomsen <mthomsen1138@hotmail.com>
2019-04-25 17:17:15 -0500

    Steamed hams

---
+++ nav-alterno.php
@@ -0,0 +1,87 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html dir="ltr" lang="es">
+
+<head>
+    <!-- Metadata -->
+    <meta charset="UTF-8">
+    <meta content="Sarah Raquel Quintana Cortés,Arturo Alejandro Acevedo Bravo, Mathias Thomsen Cuéllar" name="author">
+    <meta content="Resuelve cualquier duda buscando en las preguntas frecuentes o contactando directamente a Bats'il Maya."
+          name="description">
+    <meta content="Tseltal, Bats'il Maya, café, miel, jabón, jabones, Chiapas, Orgánico, ayuda, faq, preguntas, contacto, dudas, respuestas, problemas"
+          name="keywords">
+    <!-- Scripts de compatibilidad -->
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!-- Escala de viewport -->
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <!-- Link a CSS -->
+    <link href="grid.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- Link a favicon -->
+    <link href="images/logos/batsil_maya_logo.svg" rel="icon">
+
+    <title>Ayuda</title>
+</head>
+<body>
+<div id="ayuda">
+    <div>
+        <header>
+            <div class="grid-header container">
+                <h1>Bats'il Maya: Ayuda</h1>
+                <div class="nav-wrapper desktop">
+                    <a class="logo" href="index.php" target="_self">
+                        <img alt="Bats'il Maya Logo" class="shadow" src="images/logos/batsil_maya_logo.svg">
+                    </a>
+                    <nav id="menu-desktop">
+                        <h2>Menú</h2>
+                        <ul class="menu-desktop-content">
+                            <li><a href="index.php" target="_self">Inicio</a></li>
+                            <li><a href="nosotros.php" target="_self">Nosotros</a></li>
+                            <li><a href="nuestro-cafe.php" target="_self">Nuestro café</a></li>
+                            <li><a href="proceso.php" target="_self">Proceso</a></li>
+                            <li><a href="productos.php" target="_self">Productos</a></li>
+                            <li><a href="noticias.php" target="_self">Noticias</a></li>
+                            <li><a class="active" href="noticias.php" target="_self">Ayuda</a></li>
+                        </ul>
+                    </nav>
+                    <ul>
+                        <li class="button outline"><a href="cuenta-informacion.php" target="_self">Cuenta</a></li>
+                        <li class="button red carrito"><a class="light" href="carrito.php" target="_self">Carrito</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav-wrapper mobile">
+                    <a class="logo" href="index.php" target="_self">
+                        <img alt="Bats'il Maya Logo" class="shadow" src="images/logos/batsil_maya_logo.svg">
+                    </a>
+                    <nav id="menu-mobile">
+                        <input id="menu-mobile-toggle" type="checkbox">
+                        <label for="menu-mobile-toggle"><span id="menu-icon"></span></label>
+                        <div id="overlay"></div>
+                        <ul class="menu-mobile-content light-bg">
+                            <li><a href="index.php" target="_self">Inicio</a></li>
+                            <li><a href="nosotros.php" target="_self">Nosotros</a></li>
+                            <li><a href="nuestro-cafe.php" target="_self">Nuestro café</a></li>
+                            <li><a href="proceso.php" target="_self">Proceso</a></li>
+                            <li><a href="productos.php" target="_self">Productos</a></li>
+                            <li><a href="noticias.php" target="_self">Noticias</a></li>
+                            <li><a class="active" href="ayuda.php" target="_self">Ayuda</a></li>
+                            <li class="button outline fit-content"><a href="cuenta-informacion.php" target="_self">Cuenta</a></li>
+                            <li class="button red fit-content carrito"><a class="light" href="carrito.php" target="_self">Carrito</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <h2 class="h2-header">Ayuda</h2>
+            </div>
+        </header>
+    </div>
+
+</body>
+</html>
