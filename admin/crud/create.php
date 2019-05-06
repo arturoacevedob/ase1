69042f3f8107b5ba0936e8b0e0f798ea09edcf68
Sarah Quintana <sarahquintana@Sarahs-MacBook-Air.local>
2019-05-06 08:26:49 -0500

    create.php

--- admin/crud/create.php
+++ admin/crud/create.php
@@ -13,25 +13,15 @@ if(!isset($_SESSION['user'])) {
 
 if(isset($_POST['insert'])) {
     $uploadOk = 0;
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $price = $_POST['price'];
-    $brand = $_POST['brand'];
+    $name_legal = $_POST['name_legal'];
+    $name_alias = $_POST['name_alias'];
+    $giro= $_POST['giro'];
+    $client_type = $_POST['client_type'];
+    
 
-    if($_FILES['image']['name'] != "") {
-        $fileName = strtolower($_FILES['image']['name']);
-        $tempFile = $_FILES['image']['tmp_name'];
-        $fileNamePath = 'images/' . $fileName;
-
-        if(move_uploaded_file($tempFile, $fileNamePath)) {
-            $uploadOk = 1;
-        } else {
-            echo "Error al cargar el archivo.";
-        }
-    }
     
     if($uploadOk == 1) {
-        $q = "insert into products (name, description, price, brand, image) values ('$name','$description','$price','$brand','$fileNamePath')";
+        $q = "insert into clients (name_legal, name_alias, giro, client_type) values ('$name_legal','$name_alias','$giro','$client_type')";
         execute($q);
         header("Location: admin.php");
     }
@@ -44,12 +34,11 @@ if(isset($_POST['insert'])) {
     <h1>Productos</h1>
     <form action='create.php' method='post' enctype='multipart/form-data'>
         <input type='hidden' name='insert' value='insert'>
-        Nombre: <input type='text' name='name'> <br>
-        Descripción: <input type='text' name='description'> <br>
-        Precio: <input type='number' name='price' step='any'> <br>
-        Marca: <input type='text' name='brand'> <br>
-        Foto: <input type='file' name='image'> <br><br>
-        <input type='submit' value='Crear producto'>
+        Nombre Legal: <input type='text' name='name_legal'> <br>
+        Alias: <input type='text' name='name_alias'> <br>
+        Giro: <input type='text' name='giro'> <br>
+        Tipo de cliente: <input type='text' name='client_type'> <br>
+        <input type='submit' value='Guardar cliente'>
     </form>
 </body>
 </html>
