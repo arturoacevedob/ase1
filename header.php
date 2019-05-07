86909e421bc1aaa32497430cbcb4d9c0f189d02b
Mathias Thomsen <mthomsen1138@hotmail.com>
2019-05-07 16:21:16 -0500

    What? Spot It is cool

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 function renderHeader(){
     $headerUser = "
-        <div class='cuenta'><li class='fit-content kill'><a href='cuenta-informacion.php' target='_self'>Cuenta</a>
+        <div class='cuenta'><li class='fit-content'><a class='kill' href='cuenta-informacion.php' target='_self'>Cuenta</a>
         <ul id='submenu'>
                       <li><a href='#' target='_self'>Información</a></li>
                       <li><a href='#' target='_self'>Dirección de envío</a></li>
@@ -10,7 +10,7 @@ function renderHeader(){
 					  <li><a href='#' target='_self'>Cerrar sesión</a></li>
 				  </ul>
         </li></div>
-        <div class='carrito'><li class='fit-content kill'><a class='light' href='carrito.php' target='_self'>Carrito</a></li></div>"; 
+        <div class='carrito'><li class='fit-content'><a class='kill' class='light' href='carrito.php' target='_self'>Carrito</a></li></div>"; 
     
     $header = "
         <li class='button outline fit-content'><a href='iniciar_sesion.php' target='_self'>Iniciar sesión</a></li>
