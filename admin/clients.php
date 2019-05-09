a6738f85528bae843de1959ce94272cff8103fce
Mathias Thomsen <mthomsen1138@hotmail.com>
2019-05-09 17:29:18 -0500

    All that for a drop of blood

--- admin/clients.php
+++ admin/clients.php
@@ -378,7 +378,6 @@ function createProductListOptions()
         <li><a href="orders-pending-backend.php" target="_self">Pedidos</a></li>
         <li><a href="products-coffee.php" target="_self">Productos</a></li>
         <li><a href='login.php?killsession=1'>Terminar Sesión</a></li>
-        <li><a href='crud_productos/create.php'>Registro de productos</a></li>
     </ul>
 </nav>
 
