2f1535a63bee31573fe954c278b73ece77561b35
Arturo <arturoacevedob@gmail.com>
2019-05-12 13:27:56 -0500

    update_product.php now fully works.

--- admin/crud_product/create_product.php
+++ admin/crud_product/create_product.php
@@ -41,21 +41,9 @@ if (isset($_POST['insert'])) {
         $q = "insert into products (name_product, description, notes, client_type) values ('$name_product','$description','$notes','$client_type')";
         $id_product = execute($q);
 
-        if (isset($_POST['weight1'])) {
-            $q = "insert into weight_price (weight1, price1, id_product) values ('$weight1', '$price1', '$id_product')";
-            execute($q);
-        }
+        $q = "insert into weight_price (weight1, weight2, weight3, price1, price2, price3, id_product) values ('$weight1', '$weight2', '$weight3', '$price1',  '$price2',  '$price3', '$id_product')";
+        execute($q);
 
-        if (isset($_POST['weight2'])) {
-            $q = "insert into weight_price (weight2, price2, id_product) values ('$weight2', '$price2', '$id_product')";
-            execute($q);
-        }
-
-        if (isset($_POST['weight3'])) {
-            $q = "insert into weight_price (weight3, price3, id_product) values ('$weight3', '$price3', '$id_product')";
-            execute($q);
-        }
-        
         $q = "insert into images (image_path, id_product) values ('$relativePath', '$id_product')";
         execute($q);
         header("Location: ../products-coffee.php");
