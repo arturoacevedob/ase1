9c87f594240e7e45b2feb3d7f35c15d17a016d23
Arturo <arturoacevedob@gmail.com>
2019-05-11 19:14:13 -0500

    create product php things

--- admin/crud_product/create_product.php
+++ admin/crud_product/create_product.php
@@ -86,6 +86,14 @@ if (isset($_POST['insert'])) {
         </tbody>
     </table>
 
+    <div>
+        <h4>Image Gallery</h4>
+        <ul id="photos_clearing" class="clearing-thumbs" data-clearing>
+        </ul>
+        <br/>
+        <label for='photos'>Add some a photo:</label>
+        <input type="file" id="photos" name="photos[]" multiple/>
+    </div>
 
 </form>
 
