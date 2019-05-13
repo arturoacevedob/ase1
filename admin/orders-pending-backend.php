1e566e5cff3bc8946300cf9aa40668e10d9afae4
Sarah Quintana <sarahquintana@Sarahs-MacBook-Air.local>
2019-05-13 18:16:43 -0500

    pedidios

--- admin/orders-pending-backend.php
+++ admin/orders-pending-backend.php
@@ -32,7 +32,7 @@
     <div><img alt="Logo de Bats'il Maya" src="../images/logos/batsil_maya_logo.svg"></div>
     <ul>
         <li><a href="clients.php" target="_self">Clientes</a></li>
-        <li><a class="active bold" href="orders-pending-backend.php" target="_self">Pedidos</a></li>
+        <li><a class="active bold pedidos-active" href="orders-pending-backend.php" target="_self">Pedidos</a></li>
         <li><a href="products_coffee.php" target="_self">Productos</a></li>
         <li><a href="blog.php" target="_self">Blog</a></li>
     </ul>
