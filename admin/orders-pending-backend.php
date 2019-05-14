f9de2ed3b13e82fdb0f5d1ed433247c141751599
Sarah Quintana <sarahquintana@Sarahs-MacBook-Air.local>
2019-05-13 19:10:19 -0500

    cn

--- admin/orders-pending-backend.php
+++ admin/orders-pending-backend.php
@@ -37,8 +37,8 @@
         <!-- <li><a href="blog.php" target="_self">Blog</a></li> -->
     </ul>
     <ul class="lonely-ul">
-        <li><a href="" target="_self">Ayuda</a></li>
-        <li><a href='login.php?killsession=1'>Cerrar Sesión</a></li>
+        <li><a class="ayuda-disactive" href="" target="_self">Ayuda</a></li>
+        <li><a class="cerrar-disactive"href='login.php?killsession=1'>Cerrar Sesión</a></li>
     </ul>
 </nav>
 
