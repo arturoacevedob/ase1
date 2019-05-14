a5aa4509c26aa8c9c8729ee48a59ade9190f2a4c
Sarah Quintana <sarahquintana@Sarahs-MacBook-Air.local>
2019-05-14 11:11:13 -0500

    efef

--- admin/clients.php
+++ admin/clients.php
@@ -402,12 +402,12 @@ function createClientList()
     <div class="global-toolbar">
         <h1>Clientes</h1>
     </div>
-
+    </div>
     <ul class="grid-tabs">
         <li><a class="tab-disactive"href="clients-pending.php" id="clients-pending" target="_self">Pendientes</a></li>
         <li><a class="active tab-active" href="clients.php" id="clients" target="_self">Todos los clientes</a></li>
     </ul>
-
+    
     
     <div class="b-grey">
     <div class="grid-2-space-between give-me-gap">
