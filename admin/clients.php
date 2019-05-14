8455b8602e3f1dcf52a1262161b457b000ab7f1e
Sarah Quintana <sarahquintana@Sarahs-MacBook-Air.local>
2019-05-13 19:08:55 -0500

    n

--- admin/clients.php
+++ admin/clients.php
@@ -390,7 +390,7 @@ function createClientList()
 
     </ul>
     <ul class="lonely-ul">
-        <li><a href="" target="_self">Ayuda</a></li>
+        <li><a class="ayuda-disactive" href="" target="_self">Ayuda</a></li>
         <li><a href='login.php?killsession=1'>Cerrar Sesión</a></li>
     </ul>
 </nav>
