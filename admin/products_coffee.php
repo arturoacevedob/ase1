338526167ef756c63e5af3a4c8791022881fc491
MTC77 <mthomsen1138@hotmail.com>
2020-10-08 13:32:03 -0500

    Socorro

--- admin/products_coffee.php
+++ admin/products_coffee.php
@@ -146,7 +146,7 @@ function createCoffeeListOptions()
     </div>
     <ul class="grid-tabs">
         <li class="tab6"><a class="active tab-active" href="products_coffee.php" id="coffee" target="_self">Café</a></li>
-        <li class="tab7"><a class="tab-disactive"href="products-honey-backend.html" id="honey" target="_self">Miel</a></li>
+        <li class="tab7"><a class="tab-disactive"href="products_honey.php" id="honey" target="_self">Miel</a></li>
         <li class="tab8"><a class="tab-disactive"href="products-soap-backend.html" id="soap" target="_self">Jabón</a></li>
     </ul>
 
