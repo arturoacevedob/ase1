d7e5e2b78f531133975073ef1bbd84ff1bff8328
MTC77 <mthomsen1138@hotmail.com>
2021-02-02 19:47:45 -0600

    wow

--- agregar_producto.php
+++ agregar_producto.php
@@ -10,7 +10,7 @@ function compra()
     <div class='add-cart-register-to-buy'>
     <input type='submit' name='Agregar al carrito' class='button disable unavailable'>
     <!--<a class='note button red fit-content' href='contactanos.php' target='_self'> Regístrate para comprar</a>-->
-    <a class='note button red fit-content' href='https://www.amazon.com.mx/stores/page/458A7D6B-042B-46E6-AC15-6380E77F8CE0?ingress=2&visitId=65120477-d895-47a2-8a43-ebd0503e16b5&ref_=ast_bln' target='_self'> Compra en Amazon</a>
+    <a class='note button red fit-content' href='https://www.amazon.com.mx/stores/page/458A7D6B-042B-46E6-AC15-6380E77F8CE0?ingress=2&visitId=65120477-d895-47a2-8a43-ebd0503e16b5&ref_=ast_bln' target='_self'> Comprar en Amazon</a>
     </div>";
 
   if (isset($_SESSION["user"])) {
